9cda34a832290951b9a9364285ae2e71322ea908
Neon-Ape <louis@pixelwizart.de>
2017-06-02 12:50:43 +0200

    added lineChart

--- src/bubble_chart.js
+++ src/bubble_chart.js
@@ -142,13 +142,6 @@ function bubbleChart() {
 
 
   /*
-  Map data to show timing graphs
-   */
-  function createGraphs(data) {
-
-  }
-
-  /*
    * Main entry point to the bubble chart. This function is returned
    * by the parent closure. It prepares the rawData for visualization
    * and adds an svg element to the provided selector and starts the
@@ -362,6 +355,309 @@ function bubbleChart() {
 
   // return the chart function from closure.
   return chart;
+}
+
+/*
+ Map data to show timing graphs
+ */
+function lineGraph(){
+    // Constants for sizing
+    var width = 1200;
+    var height = 800;
+
+    // tooltip for mouseover functionality
+    var tooltip = floatingTooltip('gates_tooltip', 240);
+
+
+    // @v4 strength to apply to the position forces
+    var forceStrength = 0.05; //default 0.03
+
+    // These will be set in create_nodes and create_vis
+    var svg = null;
+    var points = null;
+    var nodes = [];
+
+    // Here we create a force layout and
+    // @v4 We create a force simulation now and
+    //  add forces to it.
+    var simulation = d3.forceSimulation()
+        .velocityDecay(0.15)
+        .force('x', d3.forceX().strength(forceStrength).x(center.x))
+        .force('y', d3.forceY().strength(forceStrength).y(center.y))
+        .on('tick', ticked);
+
+    // @v4 Force starts up automatically,
+    //  which we don't want as there aren't any nodes yet.
+    simulation.stop();
+
+    // Nice looking colors - no reason to buck the trend
+    // @v4 scales now have a flattened naming scheme
+    var fillColor = d3.scaleOrdinal()
+        .domain(['Reservoir Dogs', 'Pulp Fiction', 'Jackie Brown', 'Kill Bill: Vol. 1', 'Kill Bill: Vol. 2', 'Death Proof', 'Inglorious Basterds', 'Django Unchained', 'Hateful Eight'])
+        .range(['#D8341A', '#C2A225', '#14A622', '#9C4917','#D826BA','#BDC8E7','#305060','#505050', '#AAAAAA']);
+
+
+    /*
+     * This data manipulation function takes the raw data from
+     * the CSV file and converts it into an array of node objects.
+     * Each node will store data and visualization values to visualize
+     * a bubble.
+     *
+     * rawData is expected to be an array of data objects, read in from
+     * one of d3's loading functions like d3.csv.
+     *
+     * This function returns the new node array, with a node in that
+     * array for each element in the rawData input.
+     */
+    //TODO: insert grouping of Data here, now just static Object
+
+    function createNodes(curseWords) {
+
+        var myNodes = {
+            divider: 4,
+            'Reservoir Dogs': [
+                {
+                    words: {
+                        word: 3
+                    }
+                },
+                {
+                    words: {
+                        word: 10
+                    }
+                },
+                {
+                    words: {
+                        word: 7
+                    }
+                },
+                {
+                    words: {
+                        word: 22
+                    }
+                }
+                ]
+        };
+
+        return myNodes;
+
+
+    }
+
+
+    /*
+     * Main entry point to the bubble chart. This function is returned
+     * by the parent closure. It prepares the rawData for visualization
+     * and adds an svg element to the provided selector and starts the
+     * visualization creation process.
+     *
+     * selector is expected to be a DOM element or CSS selector that
+     * points to the parent element of the bubble chart. Inside this
+     * element, the code will add the SVG container for the visualization.
+     *
+     * rawData is expected to be an array of data objects as provided by
+     * a d3 loading function like d3.csv.
+     */
+    var chart = function chart(selector, rawData) {
+        // convert raw data into nodes data
+        //nodes = createNodes(rawData);
+        nodes = createNodes(rawData);
+        // Create a SVG element inside the provided selector
+        // with desired size.
+        svg = d3.select(selector)
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height);
+
+        // Bind nodes data to what will become DOM elements to represent them.
+        points = svg.selectAll('.bubble')
+            .data(nodes, function (d) { return d.id; });
+
+        // Create new circle elements each with class `bubble`.
+        // There will be one circle.bubble for each object in the nodes array.
+        // Initially, their radius (r attribute) will be 0.
+        // @v4 Selections are immutable, so lets capture the
+        //  enter selection to apply our transtition to below.
+        var pointsE = points.enter().append('circle')
+            .classed('bubble', true)
+            .attr('r', 0)
+            .attr('fill', function (d) { return fillColor(d.group); })
+            .attr('stroke', function (d) { return d3.rgb(fillColor(d.group)).darker(); })
+            .attr('stroke-width', 2)
+            .on('mouseover', showDetail)
+            .on('mouseout', hideDetail);
+
+        // @v4 Merge the original empty selection and the enter selection
+        points = points.merge(bubblesE);
+
+        // Fancy transition to make bubbles appear, ending with the
+        // correct radius
+        points.transition()
+            .duration(2000)
+            .attr('r', function (d) { return d.radius; });
+
+        // Set the simulation's nodes to our newly created nodes array.
+        // @v4 Once we set the nodes, the simulation will start running automatically!
+        simulation.nodes(nodes);
+
+        // Set initial layout to single group.
+        groupBubbles();
+    };
+
+    /*
+     * Callback function that is called after every tick of the
+     * force simulation.
+     * Here we do the acutal repositioning of the SVG circles
+     * based on the current x and y values of their bound node data.
+     * These x and y values are modified by the force simulation.
+     */
+    function ticked() {
+        bubbles
+            .attr('cx', function (d) { return d.x; })
+            .attr('cy', function (d) { return d.y; });
+    }
+
+    /*
+     * Provides a x value for each node to be used with the split by year
+     * x force.
+     */
+    function nodeYearPosX(d) {
+        return movieCenters[d.movie].x;
+    }
+
+    function nodeYearPosY(d) {
+        return movieCenters[d.movie].y;
+    }
+
+    function nodeGroupPos(d) {
+        return groupCenters[d.group].y;
+    }
+
+    /*
+     * Sets visualization in "single group mode".
+     * The year labels are hidden and the force layout
+     * tick function is set to move all nodes to the
+     * center of the visualization.
+     */
+    function groupBubbles() {
+        hideYearTitles();
+
+        // @v4 Reset the 'x' force to draw the bubbles to the center.
+        simulation.force('x', d3.forceX().strength(forceStrength).x(center.x));
+        simulation.force('y', d3.forceY().strength(forceStrength).y(center.y));
+
+        // @v4 We can reset the alpha value and restart the simulation
+        simulation.alpha(1).restart();
+    }
+
+
+    /*
+     * Sets visualization in "split by year mode".
+     * The year labels are shown and the force layout
+     * tick function is set to move nodes to the
+     * yearCenter of their data's year.
+     */
+    function splitBubbles() {
+        showYearTitles();
+
+        // @v4 Reset the 'x' force to draw the bubbles to their year center
+        simulation.force('x', d3.forceX().strength(forceStrength).x(nodeYearPosX));
+        simulation.force('y', d3.forceY().strength(forceStrength).y(nodeYearPosY));
+
+
+        // @v4 We can reset the alpha value and restart the simulation
+        simulation.alpha(1).restart();
+    }
+
+    function vertSplitBubbles() {
+        // @v4 Reset the 'x' force to draw the bubbles to their year center
+        simulation.force('y', d3.forceY().strength(forceStrength).y(nodeGroupPos));
+
+
+        // @v4 We can reset the alpha value and restart the simulation
+        simulation.alpha(1).restart();
+
+    }
+    /*
+     * Hides Year title displays.
+     */
+    function hideYearTitles() {
+        svg.selectAll('.year').remove();
+    }
+
+    /*
+     * Shows Year title displays.
+     */
+    function showYearTitles() {
+        // Another way to do this would be to create
+        // the year texts once and then just hide them.
+        var yearsData = d3.keys(movieTitleX);
+        var years = svg.selectAll('.year')
+            .data(yearsData);
+
+        years.enter().append('text')
+            .attr('class', 'year')
+            .attr('x', function (d) { return movieTitleX[d]; })
+            .attr('y', 40)
+            .attr('text-anchor', 'middle')
+            .text(function (d) { return d; });
+    }
+
+    /*
+     * Function called on mouseover to display the
+     * details of a bubble in the tooltip.
+     */
+    function showDetail(d) {
+        // change outline to indicate hover state.
+        d3.select(this).attr('stroke', 'black');
+
+        var content = '<span class="name">Word: </span><span class="value">' +
+            d.name +
+            '</span><br/>' +
+            '<span class="name">Amount: </span><span class="value">' +
+            d.value +
+            '</span><br/>' +
+            '<span class="name">Movie: </span><span class="value">' +
+            d.movie +
+            '</span><br/>' +
+            '<span class="name">Year: </span><span class="value">' +
+            d.year +
+            '</span>';
+
+        tooltip.showTooltip(content, d3.event);
+    }
+
+    /*
+     * Hides tooltip
+     */
+    function hideDetail(d) {
+        // reset outline
+        d3.select(this)
+            .attr('stroke', d3.rgb(fillColor(d.group)).darker());
+
+        tooltip.hideTooltip();
+    }
+
+    /*
+     * Externally accessible function (this is attached to the
+     * returned chart function). Allows the visualization to toggle
+     * between "single group" and "split by year" modes.
+     *
+     * displayName is expected to be a string and either 'year' or 'all'.
+     */
+    chart.toggleDisplay = function (displayName) {
+        if (displayName === 'year') {
+            splitBubbles();
+        } else if (displayName === 'group') {
+            vertSplitBubbles();
+        } else {
+            groupBubbles();
+        }
+    };
+
+
+    // return the chart function from closure.
+    return chart;
 }
 
 /*
