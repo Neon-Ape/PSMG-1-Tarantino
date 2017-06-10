1a7b33b602fdb4a4e8def5f2c6665ae9afd14953
Neon-Ape <louis@pixelwizart.de>
2017-06-10 13:38:54 +0200

    split files, linegraph stuff
    Linegraphs can be toggled by movie, nodes respond
    links are broken, no idea why
    x and y attr in html never get set somehow

---
+++ src/generateData.js
@@ -0,0 +1,225 @@
+/*
+ * Below is the initialization code as well as some helper functions
+ * to create a new bubble chart instance, load the data, and display it.
+ */
+
+
+var myBubbleChart = bubbleChart();
+var myLineGraph = lineGraph();
+
+function runtimeLookup(data) {
+    var runtimes = {};
+    for (var i = 0; i < data.length; i++) {
+        runtimes[data[i]["movie"]] = Number(data[i]["runtime"]);
+    }
+    return runtimes;
+}
+
+function makeTiming(data, extraData) {
+
+    var runTimes = runtimeLookup(extraData);
+
+    function MovieBlock(runtime) {
+        this.runtime = runtime;
+        this.children = {};
+    }
+
+    var timeLine = {
+    };
+
+    movieBlockActive = "";
+
+    for(var i = 0; i < data.length; i++) {
+        var currentType = data[i]["type"];
+        var currentWord = data[i]["word"];
+        var currentTime = data[i]["minutes_in"];
+        var currentMovie = data[i]["movie"];
+
+        if (currentMovie !== movieBlockActive) {
+            movieBlockActive = currentMovie;
+            var runtime = runTimes[movieBlockActive];
+            timeLine[currentMovie] = new MovieBlock(runtime);
+        }
+
+        if (currentType === "word") {
+            timeLine[movieBlockActive].children[currentTime] = currentWord;
+        }
+    }
+    console.log(timeLine);
+    return timeLine;
+}
+
+function makeCurseWords(data, movieDates) {
+
+    var curseWords = {
+        children : []
+    };
+
+    var wordCheck = [];
+
+    for (var i = 0; i <  data.length; i++) {
+        var currentWord = data[i]["word"];
+        var currentMovie = data[i]["movie"];
+
+        if (currentWord === ""){
+            // do nothing
+        } else {
+            curseWords = checkForDuplicates(curseWords, movieDates, wordCheck, currentWord, currentMovie);
+        }
+    }
+
+    return curseWords;
+}
+
+function curseGroups(word) {
+    var curseCategories = {
+        // word related
+        "fuck": "fuck",
+        "ass": "ass",
+        "shit": "shit",
+        "merde" : "shit",
+        // racial slurs
+        "n-word": "racial",
+        "negro": "racial",
+        "jap": "racial",
+        "gook": "racial",
+        "jew": "racial",
+        // genital related
+        "dick": "genital",
+        "cocksucker": "genital",
+        "pussy": "genital",
+        "cunt": "genital",
+        // blasphemy
+        "god": "blasphemy",
+        "damn": "blasphemy",
+        "hell": "blasphemy"
+    };
+
+    var group;
+    for(group in curseCategories) {
+        if(word.includes(group)) {
+            return curseCategories[group];
+        }
+    }
+    return "other";
+
+}
+
+function checkForDuplicates(curseWords, movieDates, wordCheck, currentWord, currentMovie) {
+
+    function CurseWord(word, count, movie, year, group) {
+        this.name = word;
+        this.value = count;
+        this.movie = movie;
+        this.year = year;
+        this.group = group;
+    }
+
+
+
+    var start = 0;
+    while(true) {
+        var index = wordCheck.indexOf(currentWord, start);
+
+        if(index !== -1) {
+            if(curseWords.children[index].movie !== currentMovie) {
+                start = index + 1;
+            } else {
+                curseWords.children[index].value++;
+                break;
+            }
+        } else {
+            var group = curseGroups(currentWord);
+            curseWords.children.push(new CurseWord(currentWord, 1, currentMovie, movieDates[currentMovie], group));
+            wordCheck.push(currentWord);
+            break;
+        }
+    }
+
+    return curseWords;
+}
+
+function getExtraData(data) {
+    var movieDates = [];
+
+    for (var i = 0; i <  data.length; i++) {
+        movieDates[data[i]["movie"]] = data[i]["year"];
+    }
+    return movieDates;
+}
+/*
+ * Function called once data is loaded from CSV.
+ * Calls bubble chart function to display inside #vis div.
+ */
+function addExtraInfo(error, data) {
+    if (error) {
+        console.log(error);
+    }
+    var extrasUrl = "https://raw.githubusercontent.com/Neon-Ape/PSMG-1-Tarantino/master/data/tarantino_extra.csv";
+    d3.csv(extrasUrl,function (error2, extraData) {
+        if (error2) {
+            console.log(error2);
+        }
+        var movieDates = getExtraData(extraData);
+        var wordTiming = makeTiming(data, extraData);
+        var curseWords = makeCurseWords(data, movieDates);
+
+
+        myLineGraph('#lineGraph', wordTiming);
+        myBubbleChart('#bubbleChart', curseWords);
+
+    });
+
+}
+
+/*
+ * Sets up the layout buttons to allow for toggling between view modes.
+ */
+function setupButtons() {
+    d3.select('#toolbar')
+        .selectAll('.button')
+        .on('click', function () {
+            // Remove active class from all buttons
+            d3.select('#toolbar').selectAll('.button').classed('active', false);
+            // Find the button just clicked
+            var button = d3.select(this);
+
+            // Set it as the active button
+            button.classed('active', true);
+
+            // Get the id of the button
+            var buttonId = button.attr('id');
+
+            // Toggle the bubble chart based on
+            // the currently clicked button.
+            myBubbleChart.toggleDisplay(buttonId);
+        });
+
+    d3.select('#graphSelect')
+        .selectAll('.button')
+        .on('click', function () {
+
+            // Find the button just clicked
+            var button = d3.select(this);
+
+            // Toggle the active state
+            if (button.classed('active')) {
+                button.classed('active', false);
+            } else {
+                button.classed('active', true);
+            }
+
+            // Get the id of the button
+            var buttonId = button.attr('id');
+
+            // Toggle the bubble chart based on
+            // the currently clicked button.
+            myLineGraph.toggleDisplay(buttonId);
+        });
+}
+
+// Load the data.
+d3.csv('https://raw.githubusercontent.com/Neon-Ape/PSMG-1-Tarantino/master/data/tarantino.csv', addExtraInfo);
+
+// setup the buttons.
+setupButtons();
